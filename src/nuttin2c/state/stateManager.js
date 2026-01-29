8abc47d25065100cd375aae1139173f8da08bae0
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2026-01-30 01:43:25 +1100

    Refactor add error state.

--- src/nuttin2c/state/stateManager.js
+++ src/nuttin2c/state/stateManager.js
@@ -3,31 +3,44 @@ import { Method } from "coreutil_v1";
 /**
  * StateManager
  * 
- * @template T
+ * @template T, T2
  */
 export class StateManager {
 
     constructor() {
         /** @type {Map<String, T>} */
-        this.objectMap = new Map();
+        this.domainMap = new Map();
+
+        /** @type {T2} */
+        this.error = null;
 
         /** @type {Map<String, Array<Method>} */
-        this.listeners = new Map();
+        this.domainListeners = new Map();
+
+        /** @type {Map<String, Array<Method>} */
+        this.errorListeners = new Map();
 
         /** @type {boolean} */
         this.initialized = false;
     }
 
     /**
-     * 
-     * @param {Method} listener 
+     * @param {Method} domainListener
+     * @param {Method} errorListener
      */
-    react(listener) {
+    react(domainListener, errorListener = null) {
         const anyKey = "__ANY__";
-        if (!this.listeners.has(anyKey)) {
-            this.listeners.set(anyKey, new Array());
+        if (!this.domainListeners.has(anyKey)) {
+            this.domainListeners.set(anyKey, new Array());
         }
-        this.listeners.get(anyKey).push(listener);
+        this.domainListeners.get(anyKey).push(domainListener);
+
+        if (errorListener != null) {
+            if (!this.errorListeners.has(anyKey)) {
+                this.errorListeners.set(anyKey, new Array());
+            }
+            this.errorListeners.get(anyKey).push(errorListener);
+        }
     }
 
     /**
@@ -35,65 +48,111 @@ export class StateManager {
      * @param {string} key 
      * @param {Method} listener 
      */
-    reactTo(key, listener) {
-        if (!this.listeners.has(key)) {
-            this.listeners.set(key, new Array());
+    reactTo(key, domainListener, errorListener = null) {
+        if (!this.domainListeners.has(key)) {
+            this.domainListeners.set(key, new Array());
         }
-        this.listeners.get(key).push(listener);
+        this.domainListeners.get(key).push(domainListener);
+
+        if (errorListener != null) {
+            if (!this.errorListeners.has(key)) {
+                this.errorListeners.set(key, new Array());
+            }
+            this.errorListeners.get(key).push(errorListener);
+        }
     }
 
     get objectArray() {
-        return Array.from(this.objectMap.values());
+        return Array.from(this.domainMap.values());
+    }
+
+    /**
+     * @param {Promise<T>} object
+     */
+    async handle(objectPromise, key = "__DEFAULT__") {
+        try {
+            const object = await objectPromise;
+            return await this.updateDomain(object, key);
+        } catch (error) {
+            await this.updateError(error, key);
+            return error;
+        }
+    }
+
+    async updateError(error, key = "__DEFAULT__") {
+        this.initialized = true;
+        this.error = error;
+        this.signalErrorChange(error, key);
     }
 
     /**
      * Update the state
      * 
+     * @param {any} object 
      * @param {string} key 
      * @param {T} object 
      */
-    async update(object, key = "__DEFAULT__") {
+    async updateDomain(object, key = "__DEFAULT__") {
         if (Array.isArray(object)) {
             for (let i = 0; i < object.length; i++) {
                 object[i] = this.createProxy(object[i], key, this);
             }
         }
         object = this.createProxy(object, key, this);
-        this.objectMap.set(key, object);
+        this.domainMap.set(key, object);
         this.initialized = true;
-        this.signalStateChange(object, key);
+        if (this.error != null) {
+            this.error = null;
+            this.signalErrorChange(null, key);
+        }
+        this.signalDomainChange(object, key);
         return object;
     }
 
     async delete(key = "__DEFAULT__") {
-        this.objectMap.delete(key);
-        this.listeners.delete(key);
+        this.domainMap.delete(key);
+        this.domainListeners.delete(key);
         this.initialized = true;
-        this.signalStateChange(null, key);
+        this.signalDomainChange(null, key);
     }
 
     async clear() {
         this.initialized = true;
-        for (let key of this.objectMap.keys()) {
-            this.signalStateChange(null, key);
+        for (let key of this.domainMap.keys()) {
+            this.signalDomainChange(null, key);
         }
-        this.signalStateChange(null, "__ANY__");
-        this.objectMap.clear();
-        this.listeners.clear();
+        this.signalDomainChange(null, "__ANY__");
+        this.domainMap.clear();
+        this.domainListeners.clear();
         this.initialized = false;
     }
 
-    signalStateChange(object, key) {
-        if (this.listeners.has(key)) {
-            for (let listener of this.listeners.get(key)) {
+    signalDomainChange(object, key) {
+        if (this.domainListeners.has(key)) {
+            for (let listener of this.domainListeners.get(key)) {
                 listener.call([object, key]);
             }
         }
 
         const anyKey = "__ANY__";
-        if (key != anyKey && this.listeners.has(anyKey)) {
-            for (let listener of this.listeners.get(anyKey)) {
+        if (key != anyKey && this.domainListeners.has(anyKey)) {
+            for (let listener of this.domainListeners.get(anyKey)) {
                 listener.call([object, key]);
+            }
+        }
+    }
+
+    signalErrorChange(error, key) {
+        if (this.errorListeners.has(key)) {
+            for (let listener of this.errorListeners.get(key)) {
+                listener.call([error, key]);
+            }
+        }
+
+        const anyKey = "__ANY__";
+        if (key != anyKey && this.errorListeners.has(anyKey)) {
+            for (let listener of this.errorListeners.get(anyKey)) {
+                listener.call([error, key]);
             }
         }
     }
@@ -105,7 +164,7 @@ export class StateManager {
                     return true;
                 }
                 const success = (target[prop] = value);
-                stateManager.signalStateChange(target, key);
+                stateManager.signalDomainChange(target, key);
                 return success === value;
             }
         });

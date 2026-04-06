01930542848706b542223be74d4ef0df93238e04
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2026-04-07 08:19:51 +1000

    Added Css3StylesheetBuilder.

---
+++ src/nuttin2c/styles/css3StylesheetBuilder.js
@@ -0,0 +1,342 @@
+import { StyleMedia } from "./styleMedia.js";
+import { StyleSelector } from "./styleSelector.js";
+import { StylesheetBuilder } from "./stylesheetBuilder.js";
+
+export class Css3StylesheetBuilder {
+
+    /**
+     * 
+     * @returns {Css3StylesheetBuilder}
+     */
+    static create() {
+        return new Css3StylesheetBuilder();
+    }
+
+    constructor() {
+
+        /** @type {StylesheetBuilder} */
+        this.stylesheetBuilder = StylesheetBuilder.create();
+
+    }
+
+    /**
+     * @param {String} selector 
+     * @returns {Css3StylesheetBuilder}
+     */
+    selector(selector) {
+        if (this.stylesheetBuilder.context instanceof StyleSelector) {
+            this.stylesheetBuilder.close();
+        }
+        this.stylesheetBuilder.selector(selector);
+        this.stylesheetBuilder.open();
+        return this;
+    }
+
+    /**
+     * @param {String} minWidth 
+     * @param {String} maxWidth 
+     * @returns {Css3StylesheetBuilder}
+     */
+    media(minWidth, maxWidth) {
+        if (this.stylesheetBuilder.context instanceof StyleSelector) {
+            this.stylesheetBuilder.close();
+        }
+        if (this.stylesheetBuilder.context instanceof StyleMedia) {
+            this.stylesheetBuilder.close();
+        }
+        const mediaQuery = `@media${minWidth ? ` (min-width: ${minWidth})` : ""}${minWidth && maxWidth ? " and" : ""}${maxWidth ? ` (max-width: ${maxWidth})` : ""}`;
+        this.stylesheetBuilder.media(mediaQuery);
+        this.stylesheetBuilder.open();
+        return this;
+    }
+
+    /**
+     * @param {String} width 
+     * @returns {Css3StylesheetBuilder}
+     */
+    width(width) {
+        this.stylesheetBuilder.style("width", width);
+        return this;
+    }
+
+    /**
+     * @param {String} minWidth 
+     * @returns {Css3StylesheetBuilder}
+     */
+    minWidth(minWidth) {
+        this.stylesheetBuilder.style("min-width", minWidth);
+        return this;
+    }
+
+    /**
+     * @param {String} maxWidth 
+     * @returns {Css3StylesheetBuilder}
+     */
+    maxWidth(maxWidth) {
+        this.stylesheetBuilder.style("max-width", maxWidth);
+        return this;
+    }
+
+    /**
+     * @param {String} height 
+     * @returns {Css3StylesheetBuilder}
+     */
+    height(height) {
+        this.stylesheetBuilder.style("height", height);
+        return this;
+    }
+
+    /**
+     * @param {String} minHeight 
+     * @returns {Css3StylesheetBuilder}
+     */
+    minHeight(minHeight) {
+        this.stylesheetBuilder.style("min-height", minHeight);
+        return this;
+    }
+    
+    /**
+     * @param {String} maxHeight 
+     * @returns {Css3StylesheetBuilder}
+     */
+    maxHeight(maxHeight) {
+        this.stylesheetBuilder.style("max-height", maxHeight);
+        return this;
+    }
+
+    /**
+     * @param {String} lineHeight
+     * @returns {Css3StylesheetBuilder}
+     */
+
+    lineHeight(lineHeight) {
+        this.stylesheetBuilder.style("line-height", lineHeight);
+        return this;
+    }
+
+    /**
+     * @param {String} fontSize
+     * @returns {Css3StylesheetBuilder}
+     */
+    fontSize(fontSize) {
+        this.stylesheetBuilder.style("font-size", fontSize);
+        return this;
+    }
+
+    /**
+     * @param {String} grow Grows relative to the other flex items in the container when there is extra space. E.g. grow of 1 vs grow of 2, the second item will grow twice as much.
+     * @param {String} shrink Shrinks relative to the other flex items in the container when not enough space. E.g. shrink of 1 vs shrink of 2, the second item will shrink twice as much.
+     * @param {String} basis Initial main size. E.g "100px", "20%", "auto". Default value is "auto", sized according to its content. If "0", sized based on grow and shrink.
+     * @returns {Css3StylesheetBuilder}
+     */
+    flex(grow, shrink, basis) {
+        const value = `${grow} ${shrink} ${basis}`;
+        this.stylesheetBuilder.style("flex", value);
+        return this;
+    }
+
+    /**
+     * @param {String} direction E.g "row", "column", "row-reverse", "column-reverse"
+     * @returns {Css3StylesheetBuilder}
+     */
+    flexDirection(direction) {
+        this.stylesheetBuilder.style("flex-direction", direction);
+        return this;
+    }
+
+    /**
+     * @param {String} alignItems E.g. "stretch", "center", "flex-start", "flex-end", "baseline"
+     * @returns {Css3StylesheetBuilder}
+     */
+    alignItems(alignItems) {
+        this.stylesheetBuilder.style("align-items", alignItems);
+        return this;
+    }
+
+    /**
+     * @param {String} position E.g. "static", "relative", "absolute", "fixed", "sticky"
+     * @returns {Css3StylesheetBuilder}
+     */
+    position(position) {
+        this.stylesheetBuilder.style("position", position);
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} bottom 
+     * @returns {Css3StylesheetBuilder}
+     */
+    bottom(bottom) {
+        this.stylesheetBuilder.style("bottom", bottom);
+        return this;
+    }
+    
+    /**
+     * 
+     * @param {String} right 
+     * @returns {Css3StylesheetBuilder}
+     */
+    right(right) {
+        this.stylesheetBuilder.style("right", right);
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} top 
+     * @returns {Css3StylesheetBuilder}
+     */
+    top(top) {
+        this.stylesheetBuilder.style("top", top);
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} left 
+     * @returns {Css3StylesheetBuilder}
+     */
+    left(left) {
+        this.stylesheetBuilder.style("left", left);
+        return this;
+    }
+
+    /**
+     * @param {String} display E.g. "block", "inline-block", "flex", "inline-flex", "grid", "inline-grid", "none"
+     * @returns {Css3StylesheetBuilder}
+     */
+    display(display) {
+        this.stylesheetBuilder.style("display", display);
+        return this;
+    }
+
+    /**
+     * @param {String} top 
+     * @param {String} right 
+     * @param {String} bottom 
+     * @param {String} left 
+     * @returns {Css3StylesheetBuilder}
+     */
+    padding(top, right, bottom, left) {
+        if (top && !right && !bottom && !left) {
+            const value = `${top} ${right} ${bottom} ${left}`;
+            this.stylesheetBuilder.style("padding", value);
+            return this;
+        }
+        if (top) {
+            this.stylesheetBuilder.style("padding-top", top);
+        }
+        if (right) {
+            this.stylesheetBuilder.style("padding-right", right);
+        }
+        if (bottom) {
+            this.stylesheetBuilder.style("padding-bottom", bottom);
+        }
+        if (left) {
+            this.stylesheetBuilder.style("padding-left", left);
+        }
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} top 
+     * @param {String} right 
+     * @param {String} bottom 
+     * @param {String} left 
+     * @returns {Css3StylesheetBuilder}
+     */
+    margin(top, right, bottom, left) {
+        if (top && !right && !bottom && !left) {
+            const value = `${top} ${right} ${bottom} ${left}`;
+            this.stylesheetBuilder.style("margin", value);
+            return this;
+        }
+        if (top) {
+            this.stylesheetBuilder.style("margin-top", top);
+        }
+        if (right) {
+            this.stylesheetBuilder.style("margin-right", right);
+        }
+        if (bottom) {
+            this.stylesheetBuilder.style("margin-bottom", bottom);
+        }
+        if (left) {
+            this.stylesheetBuilder.style("margin-left", left);
+        }
+        return this;
+    }
+
+    /**
+     * @param {String} color 
+     * @returns {Css3StylesheetBuilder}
+     */
+    backgroundColor(color) {
+        this.stylesheetBuilder.style("background-color", color);
+        return this;
+    }
+
+    /**
+     * @param {String} background
+     * @returns {Css3StylesheetBuilder}
+     */
+    background(background) {
+        this.stylesheetBuilder.style("background", background);
+        return this;
+    }
+
+    /**
+     * @param {String} backgroundPosition
+     * @returns {Css3StylesheetBuilder}
+     */
+    backgroundPosition(backgroundPosition) {
+        this.stylesheetBuilder.style("background-position", backgroundPosition);
+        return this;
+    }
+
+    /**
+     * @param {String} overflowX E.g. "visible", "hidden", "scroll", "auto"
+     * @param {String} overflowY E.g. "visible", "hidden", "scroll", "auto"
+     * @returns {Css3StylesheetBuilder}
+     */
+    overflow(overflowX, overflowY) {
+        if (overflowX) {
+            this.stylesheetBuilder.style("overflow-x", overflowX);
+        }
+        if (overflowY) {
+            this.stylesheetBuilder.style("overflow-y", overflowY);
+        }
+        return this;
+    }
+
+    /**
+     * @param {String} boxSizing E.g. "content-box", "border-box"
+     * @returns {Css3StylesheetBuilder}
+     */
+    boxSizing(boxSizing) {
+        this.stylesheetBuilder.style("box-sizing", boxSizing);
+        return this;
+    }
+
+    /**
+     * @param {String} property
+     * @param {String} duration
+     * @param {String} timingFunction
+     * @returns {Css3StylesheetBuilder}
+     */
+
+    transition(property, duration, timingFunction) {
+        const value = `${property} ${duration} ${timingFunction}`;
+        this.stylesheetBuilder.style("transition", value);
+        return this;
+    }
+
+    build() {
+        if (this.stylesheetBuilder.context !== null) {
+            this.stylesheetBuilder.close(); 
+        }
+        return this.stylesheetBuilder.build();
+    }
+
+}
